8f321436f15f71f5d41577539bc01c5ef6347f43
archjain <jain.archit04@yahoo.com>
2016-05-23 01:00:11 -0700

    adding some hardcoded data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,5 +2,28 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
+ var formattedName =  HTMLheaderName.replace("%data%","Archit Jain");	
+ var formattedRole =  HTMLheaderRole.replace("%data%", "Software Developer"); 
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
 
- $("#main").append([yourname]);
+ //adding bio
+ 
+ var skills = ["Java", "JS","HTML"];
+ var bio = {
+	name : "Archit Jain",
+	role : "software developer",
+	pictureurl : "images/dp.jpg",
+	skills : skills,
+	welcomemsg : "Hey ! sup"
+
+
+
+ };
+ var welcomeMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomemsg);
+ var formattedBioPic = HTMLbioPic.replace("%data%",bio.pictureurl);
+ var skillset = HTMLskills.replace("%data%",bio.skills);
+
+ $("#header").append(welcomeMsg);
+ $("#header").append(formattedBioPic);
+ $("#header").append(skillset);
