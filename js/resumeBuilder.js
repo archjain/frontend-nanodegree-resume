0ef62c74c8862adb4044ce6cb4111cff63464e92
archjain <jain.archit04@yahoo.com>
2016-05-23 13:15:49 -0700

    resume related json objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,28 +2,72 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- var formattedName =  HTMLheaderName.replace("%data%","Archit Jain");	
- var formattedRole =  HTMLheaderRole.replace("%data%", "Software Developer"); 
- $("#header").prepend(formattedRole);
- $("#header").prepend(formattedName);
+var bio = {
+	"name":"Archit Jain",
+	"role":"Software Developer",
+	"image":"images/dp.jpg"
+}
 
- //adding bio
- 
- var skills = ["Java", "JS","HTML"];
- var bio = {
-	name : "Archit Jain",
-	role : "software developer",
-	pictureurl : "images/dp.jpg",
-	skills : skills,
-	welcomemsg : "Hey ! sup"
+var workex = {
+
+	"employers":[
+	{
+		"company":"MFS",
+		"start_year":2012,
+		"end_year":2015,
+		"title":"ASE"
+	},
+	{
+		"company":"Misys",
+		"start_year":2012,
+		"end_year":2012,
+		"title":"Intern"
+	}
+
+	]
+}
+
+
+var education = {
+
+"schools":[
+{
+	"school":"Arizona State University",
+	"degree":"Masters",
+	"major":"Computer Science"
+},
+{
+	"school":"VIT",
+	"degree":"Bachelors",
+	"major":"Computer Science"
+}
+
+],
+
+"online_course":[
+  { 
+  	"course_name":"Javascript",
+  	"website":"udacity"
+  }
+
+]
+
+} 
+
+var projects = {
+  
+  "projs":[
+   {
+  		"title":"Search Engine",
+  		"description":"implemneted page rank algorithm"
+  	},
+  	{
+  		"title":"GradeBook Application",
+  		"description":"done by implementing REST API"
+  	}
+
+  ]
 
 
 
- };
- var welcomeMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomemsg);
- var formattedBioPic = HTMLbioPic.replace("%data%",bio.pictureurl);
- var skillset = HTMLskills.replace("%data%",bio.skills);
-
- $("#header").append(welcomeMsg);
- $("#header").append(formattedBioPic);
- $("#header").append(skillset);
+}
